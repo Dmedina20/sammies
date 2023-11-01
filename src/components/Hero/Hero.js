91ffd56888c6408780c5ff7ee55e89688e4770d8
Daniel Medina <dezon177@gmail.com>
2023-10-31 20:54:03 -0400

    Added Bubble animations and Hero character

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,66 @@
 import "../../App.css";
+import anya from "../../images/anya.png";
+import circles from "../../images/circles.svg";
 function Hero() {
   return (
     <>
       <div className="hero min-h-screen overflow-x-hidden bg-primary">
-        <div className="hero-overlay bg-opacity-0"></div>
-        <div className="hero-content text-center text-neutral-content">
+        <div className="hero-overlay bg-opacity-0">
+          <div className="bubbles  w-screen h-screen overflow-hidden">
+            <div>
+              <span className="bubble"></span>
+            </div>
+            <div>
+              <span className="bubble"></span>
+            </div>
+            <div>
+              <span className="bubble"></span>
+            </div>
+            <div>
+              <span className="bubble"></span>
+            </div>
+            <div>
+              <span className="bubble"></span>
+            </div>
+            <div>
+              <span className="bubble"></span>
+            </div>
+            <div>
+              <span className="bubble"></span>
+            </div>
+            <div>
+              <span className="bubble"></span>
+            </div>
+            <div>
+              <span className="bubble"></span>
+            </div>
+            <div>
+              <span className="bubble"></span>
+            </div>
+            <div>
+              <span className="bubble"></span>
+            </div>
+            <div>
+              <span className="bubble"></span>
+            </div>
+            <div>
+              <span className="bubble"></span>
+            </div>
+            <div>
+              <span className="bubble"></span>
+            </div>
+            <div>
+              <span className="bubble"></span>
+            </div>
+          </div>
+        </div>
+        <img
+          src={circles}
+          alt="bg-circles"
+          className="circles w-screen
+           h-screen hidden lg:block"
+        />
+        <div className="hero-content text-center lg:text-left text-neutral-content">
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
             <p className="mb-5">
@@ -14,6 +70,7 @@ function Hero() {
             </p>
             <button className="btn btn-accent">SHOP NOW</button>
           </div>
+          <img src={anya} alt="anya" className="content-end hidden lg:block " />
         </div>
       </div>
     </>
