7a67d7b01867e4c51035e627a745c1f3303dd044
Daniel Medina <dezon177@gmail.com>
2023-11-18 11:26:48 -0500

    cartitem component updated

--- src/components/Cards/CartItem.js
+++ src/components/Cards/CartItem.js
@@ -1,5 +1,39 @@
 import React from "react";
 
 export const CartItem = ({ item }) => {
-  return <div>CartItem</div>;
+  return (
+    <li key={item.id} className="flex py-6">
+      <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+        <img
+          src={item.image}
+          alt="product img"
+          className="h-full w-full object-cover object-center"
+        />
+      </div>
+
+      <div className="ml-4 flex flex-1 flex-col">
+        <div>
+          <div className="flex justify-between text-base font-medium text-gray-900">
+            <h3>
+              <a href={item.href}>{item.name}</a>
+            </h3>
+            <p className="ml-4">{item.price}</p>
+          </div>
+          <p className="mt-1 text-sm text-gray-500">{item.color}</p>
+        </div>
+        <div className="flex flex-1 items-end justify-between text-sm">
+          <p className="text-gray-500">Qty {item.quantity}</p>
+
+          <div className="flex">
+            <button
+              type="button"
+              className="font-medium text-indigo-600 hover:text-indigo-500"
+            >
+              Remove
+            </button>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
 };
